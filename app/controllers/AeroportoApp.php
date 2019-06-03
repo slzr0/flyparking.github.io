93f534773fc385e6ae8062dfb86ebec66ee6f0b5
andersonluissalazar <andersonluissalazar@outlook.com>
2019-06-02 23:14:59 -0300

    update: comentarios aeroporto

--- app/controllers/AeroportoApp.php
+++ app/controllers/AeroportoApp.php
@@ -16,7 +16,7 @@ namespace controllers{
     }
     /*
     Lista
-    Listando os usuarios do app
+    Listando os aeroportos
     */
     public function lista(){
       global $app;
@@ -28,7 +28,7 @@ namespace controllers{
     /*
     Get
     param $id
-    pega usuario pelo id
+    pega aeroporto pelo id
     */
     public function get($id){
       global $app;
@@ -38,66 +38,6 @@ namespace controllers{
       $resultado = $sth->fetch(\PDO::FETCH_ASSOC);
       $app->render('default.php',["data"=>$resultado],200);
     }
-    /*
-    Post
-    Cadastra novo usuario
-    */
-    /*public function add(){
-      global $app;
-      $dados = json_decode($app->request->getBody(), true);
-			$dados = (sizeof($dados)==0)? $_POST : $dados;
-			$keys = array_keys($dados); //Paga as chaves do array
-			/*
-			O uso de prepare e bindValue é importante para se evitar SQL Injection
-			*/
-			/*$sth = $this->PDO->prepare("INSERT INTO usuarios_app (".implode(',', $keys).") VALUES (:".implode(",:", $keys).")");
-			foreach ($dados as $key => $value) {
-				$sth->bindValue(':'.$key,$value);
-			}
-			$sth->execute();
-			//Retorna o id inserido
-      $app->render('default.php',["data"=>['id'=>$this->PDO->lastInsertId()]],200);
-      
-    }*/
-
-
-
-    /*
-		editar
-		param $id
-		Editando pessoa
-    */
-    /*
-		public function editar($id){
-			global $app;
-			$dados = json_decode($app->request->getBody(), true);
-			$dados = (sizeof($dados)==0)? $_POST : $dados;
-			$sets = [];
-			foreach ($dados as $key => $VALUES) {
-				$sets[] = $key." = :".$key;
-			}
- 
-			$sth = $this->PDO->prepare("UPDATE usuarios_app SET ".implode(',', $sets)." WHERE id = :id");
-			$sth ->bindValue(':id',$id);
-			foreach ($dados as $key => $value) {
-				$sth ->bindValue(':'.$key,$value);
-			}
-			//Retorna status da edição
-			$app->render('default.php',["data"=>['status'=>$sth->execute()==1]],200); 
-    }*/
-    /*
-		excluir
-		param $id
-		Excluindo pessoa
-    */
-    /*
-		public function excluir($id){
-			global $app;
-			$sth = $this->PDO->prepare("DELETE FROM usuarios_app WHERE id = :id");
-			$sth ->bindValue(':id',$id);
-			$app->render('default.php',["data"=>['status'=>$sth->execute()==1]],200); 
-    }
-    */
   }
 }
 
