3a75f2069b5db922e0a299197c80fa91e45f3842
Anderson Salazar <andersonluissalazar@outlook.com>
2019-06-04 14:20:43 -0300

    fix: db connection

--- app/db/conexao.php
+++ app/db/conexao.php
@@ -1,7 +1,7 @@
 <?php
 
-//$this->PDO = new \PDO('mysql:host=localhost;dbname=flyapp_db;charset=utf8', 'root', ''); //Conexão
-$this->PDO = new \PDO('mysql:host=localhost;dbname=flyparking_db;charset=utf8', 'root', ''); //Conexão
+$this->PDO = new \PDO('mysql:host=mysql465.umbler.com;dbname=api_fly;charset=utf8', 'fly_dba', 'b62G.C.bOaO'); //Conexão
+//$this->PDO = new \PDO('mysql:host=localhost;dbname=flyparking_db;charset=utf8', 'root', ''); //Conexão
 $this->PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION); //Habilitando erros do PDO
 
 ?>
