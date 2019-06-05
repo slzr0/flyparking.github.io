c5092dde084f6acbef8874960031ff9e9cb564c3
Anderson Salazar <andersonluissalazar@outlook.com>
2019-06-05 00:34:22 -0300

    del: remove api

--- app/db/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$this->PDO = new \PDO('mysql:host=mysql465.umbler.com;dbname=api_fly;charset=utf8', 'fly_dba', 'b62G.C.bOaO'); //Conexão
-//$this->PDO = new \PDO('mysql:host=localhost;dbname=flyparking_db;charset=utf8', 'root', ''); //Conexão
-$this->PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION); //Habilitando erros do PDO
-
-?>
--- app/templates/default.php
+++
@@ -1,5 +0,0 @@
-<?php 
-  header('Content-Type: application/json; charset=utf-8');
-  header('Access-Control-Allow-Origin: *');
-  echo json_encode($data);
-?>
